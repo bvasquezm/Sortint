e52a0c81757f1ebc994b620bc8d8f7a43c6c6190
bvasquezm <bvasquezm@uc.cl>
2022-04-30 22:28:01 -0400

    first Commit!

---
+++ bogosort.py
@@ -0,0 +1,31 @@
+from cmath import exp
+import random
+
+def bogo_sort(array):
+
+    '''
+    Code source: https://python.algorithms-library.com/sorts/bogo_sort
+
+    Messy sorting algorithm based on randomized arrays, also known as Stupid Sort
+    and other names. The best and the worst algorithm there is.
+    '''
+
+    def is_sorted(array):
+        try:
+            prev_element = array[0]
+            for element in array:
+                if element < prev_element:
+                    return False
+                prev_element = element
+            return True
+
+        except (IndexError, TypeError):
+            print("Given array is either empty or not an array")
+
+    while not is_sorted(array):
+        random.shuffle(array)
+    return array
+
+if __name__ == "__main__":
+    example_array = list({1,4,3,2})
+    print(bogo_sort(example_array))
